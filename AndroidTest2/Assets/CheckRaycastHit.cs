c3209e5581d8732ff5e3f7a1761125272dbfa16c
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-10-30 09:41:48 -0600

    First commit
    FIrst update

---
+++ AndroidTest2/Assets/CheckRaycastHit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckRaycastHit : MonoBehaviour {
+
+    public Material newMaterial;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        RaycastHit hit;
+        bool crashed = Physics.Raycast(this.transform.position,
+                                        this.transform.TransformDirection(Vector3.forward),
+                                        out hit,
+                                        Mathf.Infinity
+                                        );
+        if (crashed) {
+            Debug.Log("Crashed with " + hit.collider.gameObject);
+            hit.collider.gameObject.GetComponent<Renderer>().material = newMaterial;
+        }
+	}
+}

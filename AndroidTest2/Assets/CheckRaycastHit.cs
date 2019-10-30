ccecdb1ead0e2877fd285db114a15c57e8ba437f
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-10-30 10:27:04 -0600

    Updated code
    Code ready!

--- AndroidTest2/Assets/CheckRaycastHit.cs
+++ AndroidTest2/Assets/CheckRaycastHit.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckRaycastHit : MonoBehaviour {
 
     public Material newMaterial;
+    float viewTimer;
+    GameObject selectedObject;
+    public Image image;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,9 +22,30 @@ public class CheckRaycastHit : MonoBehaviour {
                                         out hit,
                                         Mathf.Infinity
                                         );
-        if (crashed) {
-            Debug.Log("Crashed with " + hit.collider.gameObject);
-            hit.collider.gameObject.GetComponent<Renderer>().material = newMaterial;
+        if (crashed)
+        {
+             GameObject crashedObject = hit.collider.gameObject;
+            if (selectedObject == crashedObject)
+            {
+                viewTimer += Time.deltaTime;
+            }
+            else
+            {
+                viewTimer = 0;
+                selectedObject = crashedObject;
+            }
+            
+            if (viewTimer >= 5.0f) { 
+                hit.collider.gameObject.GetComponent<Renderer>().material = newMaterial;
+                Destroy(selectedObject);
+                selectedObject = null;
+                viewTimer = 0;
+            }
         }
-	}
+        else {
+            viewTimer = 0;
+            selectedObject = null;
+        }
+        image.fillAmount = viewTimer / 5.0f;
+    }
 }
